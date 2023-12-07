40b83fa1478ebc7b7ed1ad199322da4825455ec6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-12-06 19:41:28 -0500

    First Query
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+import * as schema from './schema';
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+// Can only use db in server components
+export const db = drizzle(client, { schema });
