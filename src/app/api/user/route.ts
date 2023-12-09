88bc79be8af16b0084958d0b3c1c2fd34c4dd434
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-12-08 23:23:03 -0500

    Query Operators
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,10 +1,33 @@
 import { db } from '@/db';
 import { users } from '@/db/schema';
-import { eq } from 'drizzle-orm';
+import { and } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
 export const GET = async () => {
-  const result = await db.select().from(users).where(eq(users.id, 1)).execute();
+  const result = await db
+    .select()
+    .from(users)
+    .where(and(users.score, 95))
+    .execute();
 
   return NextResponse.json(result);
 };
+
+// eq = equal
+// ne = not equal
+// gt = greater than
+// lt = less than
+// gte = greater than or =
+// lte = less than or =
+// isNull = if field is null
+// isNotNull = if field is not null
+// inArray. = if field matches one of the array elements ex. isArray(user.id, [1,2,3,4])
+// notInArray = …
+// between = between two values ex. between(user.score 50, 90)
+// notBetween = …
+// like = regex like search string (case sensitive)
+// ilike = ^ (case insensitive)
+// notLike = …
+// notILike = …
+// and = Combine a list of conditions -> Nest with or
+// or = Logical or                    -> Nest with and
